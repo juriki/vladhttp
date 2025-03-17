5b2d8a99b8fd910293dad96cfacb7177c700ad5a
Juri Tökvin <juri.tokvin@student.careeria.fi>
2025-03-17 14:43:05 +0200

    newcommit

--- netlify/functions/getDropboxToken.js
+++ netlify/functions/getDropboxToken.js
@@ -4,7 +4,10 @@ export async function handler(event) {
   const DROPBOX_REFRESH_TOKEN = process.env.DROPBOX_REFRESH_TOKEN;
   const DROPBOX_APP_KEY = process.env.DROPBOX_APP_KEY;
   const DROPBOX_APP_SECRET = process.env.DROPBOX_APP_SECRET;
-
+  console.log("DROPBOX_REFRESH_TOKEN:", DROPBOX_REFRESH_TOKEN);
+  console.log("DROPBOX_APP_KEY:", DROPBOX_APP_KEY);
+  console.log("DROPBOX_APP_SECRET:", DROPBOX_APP_SECRET);
+  
   if (!DROPBOX_REFRESH_TOKEN || !DROPBOX_APP_KEY || !DROPBOX_APP_SECRET) {
     console.error("Ошибка: Dropbox API keys are missing");
     return {
