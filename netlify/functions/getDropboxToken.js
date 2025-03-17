b78fa5776f61b03334f159f8c8aac944a98cd24e
Juri Tökvin <juri.tokvin@student.careeria.fi>
2025-03-17 14:26:38 +0200

    nextCoomit

--- netlify/functions/getDropboxToken.js
+++ netlify/functions/getDropboxToken.js
@@ -1,9 +1,12 @@
+require('dotenv').config(); // Подключаем dotenv для загрузки переменных окружения из .env
+
 export async function handler(event) {
   const DROPBOX_REFRESH_TOKEN = process.env.DROPBOX_REFRESH_TOKEN;
   const DROPBOX_APP_KEY = process.env.DROPBOX_APP_KEY;
   const DROPBOX_APP_SECRET = process.env.DROPBOX_APP_SECRET;
 
   if (!DROPBOX_REFRESH_TOKEN || !DROPBOX_APP_KEY || !DROPBOX_APP_SECRET) {
+    console.error("Ошибка: Dropbox API keys are missing");
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Dropbox API keys are missing" }),
@@ -33,6 +36,7 @@ export async function handler(event) {
       body: JSON.stringify({ access_token: data.access_token }),
     };
   } catch (error) {
+    console.error("Ошибка при запросе к Dropbox API:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
